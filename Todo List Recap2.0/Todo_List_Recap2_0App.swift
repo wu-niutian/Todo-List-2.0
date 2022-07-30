c019c308ab130e49579a65a6415f4ea85087d697
Wu Niutian <wuniutian@gmail.com>
2022-07-30 09:48:52 +0800

    Initial Commit

---
+++ Todo List Recap2.0/Todo_List_Recap2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_Recap2_0App.swift
+//  Todo List Recap2.0
+//
+//  Created by    wuniutian on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_Recap2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
